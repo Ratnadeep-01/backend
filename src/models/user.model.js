469ba3af0067f93cce36c2f0d560bf96de66053b
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-07-31 15:52:57 +0530

    fix: streamline password hashing logic in user model pre-save hook

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -46,12 +46,11 @@ const userSchema = new Schema({
     }
 }, { timestamps : true});
 
-userSchema.pre("save", async function (){
-    if(this.isModified("password")){
-        this.password = await bcrypt.hash(this.password, 10)
+userSchema.pre("save", async function () {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
     }
-    // next()
-})
+});
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
