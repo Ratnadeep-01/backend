f5e28be48ef73386707d0450d600ce7934dcf6cd
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-07-31 15:52:10 +0530

    fix: enhance tweet controller to include likes count and user like status

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,22 +1,47 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import { Like } from "../models/like.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
     const { content } = req.body
+    if (!content?.trim()) {
+        throw new ApiError(400, "Tweet content is required")
+    }
     const tweet = await Tweet.create({ content, owner: req.user._id })
-    return res.status(201).json(new ApiResponse(201, tweet, "Tweet created successfully"))
+    const populatedTweet = await Tweet.findById(tweet._id).populate("owner", "username fullName avatar")
+    return res.status(201).json(new ApiResponse(201, populatedTweet, "Tweet created successfully"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
     const { userId } = req.params
-    const tweets = await Tweet.find({ owner: userId }).populate("owner", "username fullName avatar")
-    return res.status(200).json(new ApiResponse(200, tweets, "User tweets fetched successfully"))
+    let targetUserId = userId
+    if (!isValidObjectId(userId)) {
+        const userObj = await User.findOne({ username: userId.toLowerCase() })
+        if (!userObj) {
+            return res.status(200).json(new ApiResponse(200, [], "User tweets fetched successfully"))
+        }
+        targetUserId = userObj._id
+    }
+    const tweets = await Tweet.find({ owner: targetUserId }).populate("owner", "username fullName avatar").sort({ createdAt: -1 })
+
+    const tweetsWithLikes = await Promise.all(
+        tweets.map(async (tweet) => {
+            const likesCount = await Like.countDocuments({ tweet: tweet._id })
+            const isLiked = req.user
+                ? !!(await Like.findOne({ tweet: tweet._id, likedBy: req.user._id }))
+                : false
+            const tweetData = tweet.toObject()
+            tweetData.likesCount = likesCount
+            tweetData.isLiked = isLiked
+            return tweetData
+        })
+    )
+
+    return res.status(200).json(new ApiResponse(200, tweetsWithLikes, "User tweets fetched successfully"))
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
