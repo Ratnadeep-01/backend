79d2dd79406d906c1f8f3962fe78847e49c36dc8
Ratnadeep-01 <ratnadeep4881@gmail.com>
2026-07-31 15:46:34 +0530

    fix: update getChannelStats to include total views in channel statistics

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,29 +1,40 @@
 import mongoose from "mongoose"
-import {Video} from "../models/video.model.js"
-import {Subscription} from "../models/subscription.model.js"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import { Like } from "../models/like.model.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
-    const { channelId } = req.params
-    const totalVideos = await Video.countDocuments({ owner: channelId })
-    const totalSubscribers = await Subscription.countDocuments({ owner: channelId })
-    const totalLikes = await Like.countDocuments({ owner: channelId })
+    const channelId = req.user._id
 
-    return res.status(200).json(new ApiResponse(200, { totalVideos, totalSubscribers, totalLikes }, "Channel stats fetched successfully"))
+    const totalVideos = await Video.countDocuments({ owner: channelId })
+    const totalSubscribers = await Subscription.countDocuments({ channel: channelId })
+
+    // Aggregate total views across all videos owned by channel
+    const videoViewsAggregate = await Video.aggregate([
+        { $match: { owner: new mongoose.Types.ObjectId(channelId) } },
+        { $group: { _id: null, totalViews: { $sum: "$views" } } }
+    ])
+
+    const totalViews = videoViewsAggregate[0]?.totalViews || 0
+
+    // Aggregate total likes across all videos owned by channel
+    const videoIds = await Video.find({ owner: channelId }).distinct("_id")
+    const totalLikes = await Like.countDocuments({ video: { $in: videoIds } })
+
+    return res.status(200).json(
+        new ApiResponse(200, { totalVideos, totalSubscribers, totalViews, totalLikes }, "Channel stats fetched successfully")
+    )
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
-    const { channelId } = req.params
-    const videos = await Video.find({ owner: channelId })
-    return res.status(200).json(new ApiResponse(200, videos, "Videos fetched successfully"))
+    const channelId = req.user._id
+    const videos = await Video.find({ owner: channelId }).sort({ createdAt: -1 })
+    return res.status(200).json(new ApiResponse(200, videos, "Channel videos fetched successfully"))
 })
 
 export {
-    getChannelStats, 
+    getChannelStats,
     getChannelVideos
-    }
+}
